f5b9d5a6df57155064df008d3434affa5eee703c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-11-28 22:20:39 -0500

    fifo

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -6,6 +6,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
     """ Class FIFO """
+
     def __init__(self):
         """ Initialization """
         super(FIFOCache, self).__init__()
@@ -17,6 +18,7 @@ class FIFOCache(BaseCaching):
             return None
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # print("CACHE {}".format(self.cache_data))
             last = list(self.cache_data.keys())[0]
             self.cache_data.pop(last)
             print("DISCARD: {}".format(last))
