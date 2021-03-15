50ce007908af74795c268cec4d1565604c7e9eb1
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-15 17:26:13 -0500

    return list

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Return values with the appropiate types """
+
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ Return list """
+    return [(i, len(i)) for i in lst]
