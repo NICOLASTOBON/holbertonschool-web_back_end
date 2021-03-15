593ce3d1b46c49073e790dbee11db97f5f4d60a3
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-15 15:37:13 -0500

    list of float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" return float """
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Return float """
+    return sum(mxd_lst)
