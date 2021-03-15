3da73a0414906bcaaa9a20ccced206a69a1510a7
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-15 17:01:09 -0500

    return fuction

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Return a funtion that multiplies a float """
+
+from typing import  Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ Return function """
+    return lambda num: multiplier * num
