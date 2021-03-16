b59d6001ba33bf37a893d13cf5e65ec917dbfc57
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-16 18:28:28 -0500

    return task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,6 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(n: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Return task """
-    return asyncio.create_task(wait_random(n))
+    return asyncio.create_task(wait_random(max_delay))
