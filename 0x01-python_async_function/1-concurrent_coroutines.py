6c0583a54a2082385fc52f9a239e578aac87d678
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-16 17:30:36 -0500

    return list of random numbers

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ return list """
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
