01718e7accff485660f752acec6c2ea8eb4204e1
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-16 18:09:17 -0500

    return measure

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Return measure """
 
-import time
+from time import perf_counter
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
     """ Return measure """
-    start_time = time.time()
+    start_time = perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    return time.time() - start_time
+    return perf_counter() - start_time
