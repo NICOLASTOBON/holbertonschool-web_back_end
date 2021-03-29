4b828f2398618d6211deed9072e243c72468c638
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-28 19:58:23 -0500

    add file

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -34,12 +34,8 @@ class RedactingFormatter(logging.Formatter):
         return super(RedactingFormatter, self).format(record)
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-        ) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     returns the log message
     """
