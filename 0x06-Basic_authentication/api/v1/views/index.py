8afac83662b2a72280b88339aff30574553240f7
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-29 19:24:21 -0500

    add file

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort
+    """
+    abort(403)
