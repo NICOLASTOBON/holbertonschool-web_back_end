9e66954eae42f6344c61a5e34512ce65ab4a9337
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-03-30 21:19:37 -0500

    add file

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Authorization """
 
 import base64
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -25,3 +26,15 @@ class BasicAuth(Auth):
                 return base64.b64decode(base64_authorization_header).decode()
             except base64.binascii.Error:
                 return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """
+        User credentials
+        Return: tuple
+        """
+        if decoded_base64_authorization_header and \
+                isinstance(decoded_base64_authorization_header, str):
+            if ':' in decoded_base64_authorization_header:
+                return tuple(decoded_base64_authorization_header.split(':'))
+        return (None, None)
