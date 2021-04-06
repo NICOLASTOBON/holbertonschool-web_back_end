872067d146520048f0af6cd5fd7c8d375475ec15
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-06 17:39:15 -0500

    add file

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 """ Session Authorization """
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """ Empty class """
-    pass
+    """ session class """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ method that create a session """
+        if user_id and isinstance(user_id, str):
+            id = str(uuid.uuid4())
+            self.user_id_by_session_id[id] = user_id
+            return id
+        return None
