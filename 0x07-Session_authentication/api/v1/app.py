407c9973cceace925ac13a4dd41d6f7167cba626
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-06 17:02:40 -0500

    add file

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -15,13 +15,19 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-if os.getenv('AUTH_TYPE') == 'auth':
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if os.getenv('AUTH_TYPE') == 'basic_auth':
+if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+
+if AUTH_TYPE == 'session_auth':
+    from api.v1.auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
