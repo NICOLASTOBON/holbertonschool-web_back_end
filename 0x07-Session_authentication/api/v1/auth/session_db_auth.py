251c4106f5ea4e3cfbb3fcc3e6dad58a398147f6
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-07 23:15:19 -0500

    add file

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,20 +7,28 @@ from api.v1.auth.session_exp_auth import SessionExpAuth
 class SessionDBAuth(SessionExpAuth):
     """ sesssion database """
     def create_session(self, user_id=None):
-        """Create session
+        """
+        method that create a new session
         """
         if user_id:
             session_id = super().create_session(user_id)
-            us = UserSession(user_id=user_id, session_id=session_id)
-            us.save()
-            UserSession.save_to_file()
+            user = UserSession(user_id=user_id, session_id=session_id)
+            user.save()
             return session_id
+        return None
 
     def user_id_for_session_id(self, session_id=None):
-        """ method that get user by session_id """
-        if session_id:
-            user = UserSession.search({'session_id': session_id})
-            return super().user_id_for_session_id(user[0].session_id)
+        """Get user ID from session
+        """
+        if not session_id:
+            return None
+        UserSession.load_from_file()
+        users = UserSession.search({'session_id': session_id})
+        for u in users:
+            delta = timedelta(seconds=self.session_duration)
+            if u.created_at + delta < datetime.now():
+                return None
+            return u.user_id
 
     def destroy_session(self, request=None):
         """Delete the user session / log out
