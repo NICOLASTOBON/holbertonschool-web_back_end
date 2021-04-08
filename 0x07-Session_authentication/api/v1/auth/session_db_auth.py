ce8b5794cbb12cf197579a07917b13557b13f218
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-07 23:12:13 -0500

    add file

---
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Expiratetion date """
+from models.user_session import UserSession
+from api.v1.auth.session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ sesssion database """
+    def create_session(self, user_id=None):
+        """
+        method that create a new session
+        """
+        if user_id:
+            session_id = super().create_session(user_id)
+            user = UserSession(user_id=user_id, session_id=session_id)
+            user.save()
+            return session_id
+        return None
+
+    def user_id_for_session_id(self, session_id=None):
+        """ method that get user by session_id """
+        if session_id:
+            user = UserSession.search({'session_id': session_id})
+            return super().user_id_for_session_id(user[0].session_id)
+
+    def destroy_session(self, request=None):
+        """Delete the user session / log out
+        """
+        if request:
+            session_id = self.session_cookie(request)
+            if not session_id:
+                return False
+            if not self.user_id_for_session_id(session_id):
+                return False
+            users = UserSession.search({'session_id': session_id})
+            for u in users:
+                u.remove()
+                UserSession.save_to_file()
+                return True
+        return False
