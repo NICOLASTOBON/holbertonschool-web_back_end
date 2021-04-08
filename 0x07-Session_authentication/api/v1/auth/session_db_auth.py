e18a0b0cc7f0b85d4249f4536a6ecdd9cc43e214
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-07 23:14:07 -0500

    add file

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,15 +7,14 @@ from api.v1.auth.session_exp_auth import SessionExpAuth
 class SessionDBAuth(SessionExpAuth):
     """ sesssion database """
     def create_session(self, user_id=None):
-        """
-        method that create a new session
+        """Create session
         """
         if user_id:
             session_id = super().create_session(user_id)
-            user = UserSession(user_id=user_id, session_id=session_id)
-            user.save()
+            us = UserSession(user_id=user_id, session_id=session_id)
+            us.save()
+            UserSession.save_to_file()
             return session_id
-        return None
 
     def user_id_for_session_id(self, session_id=None):
         """ method that get user by session_id """
