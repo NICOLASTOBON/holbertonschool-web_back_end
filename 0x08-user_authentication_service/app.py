ed79f5c3cfa481aaf4424f49237280356911c216
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 17:07:58 -0500

    add file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 """ Basic flask app """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def basic():
     """ Basic app """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user():
+    """ function that register an User """
+    data = dict(request.form)
+    email = data.get('email')
+    password = data.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify(
+            {"email": "{}".format(user.email), "message": "user created"}
+            ), 201
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
