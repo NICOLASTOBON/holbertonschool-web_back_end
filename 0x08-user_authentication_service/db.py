b05d929fc75dacc6fdac12016c4cb82c814a3c16
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 00:59:29 -0500

    add file

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -6,14 +6,13 @@ database file
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from typing import TypeVar
+
 from user import Base, User
 
 
 class DB:
     """ Database class"""
+
     def __init__(self):
         """ method constructor that create a connection"""
         self._engine = create_engine("sqlite:///a.db", echo=True)
