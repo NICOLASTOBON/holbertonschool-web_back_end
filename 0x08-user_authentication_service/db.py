97553a2a27debd2b22ff63f3f4748552571aa170
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 00:46:38 -0500

    add file

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -25,7 +25,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """methood that create new user """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
