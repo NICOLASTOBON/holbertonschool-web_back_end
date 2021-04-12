29fed50367fe779e2fdb7ee90859863cd9fcf580
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 00:41:48 -0500

    add file

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+database file
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base, User
+
+
+class DB:
+
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str):
+        """methood that create new user """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
