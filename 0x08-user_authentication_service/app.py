40132f56d74d3a6944da48893b2d3f61c06dc7d0
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 17:29:07 -0500

    add file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -23,7 +23,7 @@ def users() -> str:
     password = data.get('password')
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": f"{email}", "message": "user created"}), 201
+        return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
