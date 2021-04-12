c5b8082e63fec47421e355c973735323641eb376
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 02:13:04 -0500

    add file

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -36,7 +36,4 @@ class DB:
 
     def find_user_by(self, **kwargs):
         """ method that find a user """
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
-        return user
+        return self._session.query(User).filter_by(**kwargs).one()
