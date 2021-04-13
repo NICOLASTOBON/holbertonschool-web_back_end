c8e1b36ddaf05781a29bb2abe9320b2aa09d9301
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 23:40:38 -0500

    add file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -81,13 +81,15 @@ def get_reset_password_token() -> str:
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
-    """ function that update a password """
+    """ PUT /reset_password
+      Return:
+        - message
+    """
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
-    new_pwd = request.form.get('new_password')
-
+    new_password = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token, new_pwd)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
