4d372678f4ffa5d8fd920de9f8a3bc2dab4ec35e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-13 00:58:01 -0500

    add file

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -82,15 +82,16 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """ Update password """
+        """ method that update password """
         if not reset_token or not password:
             return None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            newPwd = _hash_password(password)
-            self._db.update_user(user.id, hashed_password=newPwd,
-                                 reset_token=None)
-        except NoResultFound:
+            new_pwd = _hash_password(password)
+            self._db.update_user(
+                    user.id, hashed_password=new_pwd, reset_token=None
+                )
+        except Exception:
             raise ValueError
 
 
