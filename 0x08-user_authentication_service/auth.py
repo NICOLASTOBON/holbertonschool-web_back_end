3e2d6e1db6bf8a741e512038d0e0cfcc23fdf33a
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 22:37:16 -0500

    add file

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,11 +72,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """ methoh that generate a token """
-        user = self._db.find_user_by(email=email)
-        if user:
+        try:
+            user = self._db.find_user_by(email=email)
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
-        else:
+
+            return token
+        except Exception:
             raise ValueError
 
 
