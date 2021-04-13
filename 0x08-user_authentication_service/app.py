a30bef42208c9f1f7b884d4de23004d8e12a826f
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-12 22:20:41 -0500

    add file

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -56,5 +56,16 @@ def logout() -> Response:
         abort(403)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ function profile user """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if (user):
+        return jsonify({"email": f"{user.email}"}), 200
+    else:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
