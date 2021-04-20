0cd7abfa442240f631f912f5847929e8f84c8ce8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-19 20:03:06 -0500

    add file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -9,14 +9,14 @@ import unittest
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class for testing """
+    """ Class for testing GithubOrgClient """
 
     @parameterized.expand([
         ("google"),
         ("abc")
     ])
-    @patch('client.GithubOrgClient.org')
-    def test_org(self, org_name, mock_org):
+    def test_org(self, org_name):
         """ Test function for client.GithubOrgClient.org """
-        client = GithubOrgClient(org_name)
-        self.assertEqual(client.org.return_value, mock_org.return_value)
+        with patch('client.GithubOrgClient.org') as mock_org:
+            client = GithubOrgClient(org_name=org_name)
+            self.assertEqual(client.org.return_value, mock_org.return_value)
