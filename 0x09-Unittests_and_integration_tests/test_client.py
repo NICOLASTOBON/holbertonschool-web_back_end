14e578eb65d5681e77a08b122b2794e03052f66e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-19 20:06:16 -0500

    add file

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
-""" This module tests the clients.py file """
-from unittest import mock
-from client import GithubOrgClient
-from fixtures import TEST_PAYLOAD
-from parameterized import parameterized, parameterized_class
-from unittest.mock import PropertyMock, patch
+""" test for client module """
 import unittest
+from unittest.mock import patch
+from client import GithubOrgClient
+from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class for testing GithubOrgClient """
+    """ Class for testing """
 
     @parameterized.expand([
         ("google"),
         ("abc")
     ])
-    def test_org(self, org_name):
+    @patch('client.GithubOrgClient.org')
+    def test_org(self, org_name, mock_org):
         """ Test function for client.GithubOrgClient.org """
-        with patch('client.GithubOrgClient.org') as mock_org:
-            client = GithubOrgClient(org_name=org_name)
-            self.assertEqual(client.org.return_value, mock_org.return_value)
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org.return_value, mock_org.return_value)
