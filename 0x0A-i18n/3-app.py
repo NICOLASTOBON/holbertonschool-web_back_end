c7195c86331f1e3f57358fcb36434fa247b3a97d
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-22 01:28:13 -0500

    add file

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -20,13 +20,13 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """get locale """
     return request.accept_languages.best_match(['en', 'fr'])
 
 
 @app.route('/')
-def basic_app():
+def basic_app() -> str:
     """ this is a basic app """
     data = {
         'home_title': gettext(u'home_title'),
