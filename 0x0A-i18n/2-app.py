5cbb8eb7431efa8e5c10ff785ee65ac47063f740
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-22 00:59:30 -0500

    add file

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
 """ app main """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """ class config """
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale """
+    return request.accept_languages.best_match(['en', 'fr'])
+
 
 @app.route('/')
 def basic_app():
