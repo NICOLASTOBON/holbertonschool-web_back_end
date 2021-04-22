806641d573208932b2bd527410a9c58618815e91
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-22 02:44:43 -0500

    add file

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python3
-""" This module creates a Flask app """
+"""
+App FLASK
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext
-from typing import Text
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
-    """ Config class for babel """
-
+    """
+    Config class
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+babel = Babel(app)
+
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index() -> Text:
-    """ Returns the index.html page """
-    return render_template("3-index.html")
-
-
 @babel.localeselector
-def get_locale() -> str:
-    """ Gets the locale from request.accept_languages """
+def get_locale():
+    """
+    Best match language
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home():
+    """
+    home route
+    return: template
+    """
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
