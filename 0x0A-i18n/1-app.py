ed79ff487b6b2987b0b90ef6133758aa63cff99f
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-04-22 00:09:37 -0500

    add file

--- 0x0A-i18n/1-app.py
+++ 0x0A-i18n/1-app.py
@@ -12,6 +12,8 @@ babel = Babel(app)
 class Config(object):
     """ class config """
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en',
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
