0c67a6884231f09d8691dee2c226aa58208c1080
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-11 02:18:31 -0500

    add file

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`))
-    .catch(() => new Error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
