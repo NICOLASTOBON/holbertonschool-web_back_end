705c97d80b32145e0a772a1aab405665b3993c79
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-11 03:17:22 -0500

    add file

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((value) => ({ status: 'fulfilled', value }))
     .catch((err) => ({ status: 'rejected', value: `${err.name}: ${err.message}` }));
 }
