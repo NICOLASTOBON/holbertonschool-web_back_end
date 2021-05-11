f47fbf9e7563bea1bbef065f7661ded3294be35d
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-11 01:54:17 -0500

    add file

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => new Error())
+    .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
 
