9dc45a47adeb3fb9a99e3e0fb67a4fd666d8a4ad
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-11 20:52:38 -0500

    add file

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -1,10 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
-  const photoData = await uploadPhoto();
-  const userData = await createUser();
-
-  return { photo: photoData, user: userData };
+  try {
+    const photoData = await uploadPhoto();
+    const userData = await createUser();
+    return { photo: photoData, user: userData };
+  } catch (err) {
+    return { photo: null, user: null };
+  }
 }
 
 export default asyncUploadUser;
