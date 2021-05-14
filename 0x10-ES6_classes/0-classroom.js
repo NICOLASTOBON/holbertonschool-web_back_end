38ec26c0f1b7052cb3d3d06345e641b73ee3e2aa
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-13 19:25:26 -0500

    add file

---
+++ 0x10-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_maxStudentsSize"] }] */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
