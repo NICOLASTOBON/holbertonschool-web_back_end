8dcd89c2c029391b7385c1ded424da3cc8d0f8a8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-14 00:07:14 -0500

    add file

---
+++ 0x10-ES6_classes/6-sky_high.js
@@ -0,0 +1,19 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_floors"] }] */
+import Building from './5-building';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+
+export default SkyHighBuilding;
