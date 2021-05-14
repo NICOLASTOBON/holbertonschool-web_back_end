0697c27b4797101f8e631d94b92cd3aefdbe22a8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-13 23:55:27 -0500

    add file

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+/* eslint no-underscore-dangle: ["error", { "allow": ["_sqft"] }] */
+
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
