b9d3731252bdac6dd25013648f23ab7414f56434
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-18 02:17:21 -0500

    add files

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,10 @@
-function createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   try {
     const buffer = new ArrayBuffer(length);
     const dataView = new DataView(buffer);
-
     dataView.setInt8(position, value);
-
     return dataView;
   } catch (e) {
     throw new Error('Position outside range');
   }
 }
-
-export default createInt8TypedArray;
