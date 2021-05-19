ad8ca45ba00f3b0b5e26796fbdb13e7fc624cb26
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-18 20:37:14 -0500

    add files

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,12 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') return '';
 
-  return [...set]
-    .map((item) => (typeof item === 'string' && item.startsWith(startString) ? item.slice(startString.length) : ''))
-    .filter((item) => item !== '')
-    .join('-');
+  const newString = [];
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      newString.push(value.slice(startString.length));
+    }
+  }
+
+  return newString.join('-');
 }
