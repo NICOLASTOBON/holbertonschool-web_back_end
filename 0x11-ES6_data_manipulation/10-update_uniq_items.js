e31a23e8f9ae2c4be975559d7a09f895d88535cd
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-18 22:11:39 -0500

    add files

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,6 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Object)) throw new Error('Cannot process');
+
   for (const [grocery, quantity] of map) {
     if (quantity === 1) map.set(grocery, 100);
   }
