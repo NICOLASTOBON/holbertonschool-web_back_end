0b52418703d7cb29179716ef0a730365d69f6321
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-24 20:24:13 -0500

    add files

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -3,4 +3,5 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 
-app.get('/', (req, res) => res.send('Hello Holberton School!')).listen(PORT);
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
+app.listen(PORT);
