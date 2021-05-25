5ea76aac187d70d8b1d51f7c5e49c55e5981de58
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-24 19:07:59 -0500

    add files

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const PATH = process.argv[2];
+const PORT = 1245;
+
+async function router(req, res) {
+  const { students, studentsByCS, studentsBySWE } = await countStudents(PATH);
+
+  if (req.url === '/students') {
+    res.write(`Number of students: ${students.length}\n`);
+    res.write(`Number of students in CS: ${studentsByCS.length}. List: ${studentsByCS.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${studentsBySWE.length}. List: ${studentsBySWE.join(', ')}\n`);
+    res.end();
+  } else {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+}
+
+const app = http.createServer(router).listen(PORT);
+
+module.exports = app;
