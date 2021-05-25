5e3808c9c19d5c0e3d1cb4be272877d105bdfc71
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-24 23:25:59 -0500

    add files

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -10,20 +10,19 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  countStudents(PATH)
-    .then((data) => {
-      const { students, studentsByCS, studentsBySWE } = data;
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${students.length}\n`);
-      res.write(`Number of students in CS: ${studentsByCS.length}. List: ${studentsByCS.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${studentsBySWE.length}. List: ${studentsBySWE.join(', ')}`);
+app.get('/students', async (req, res) => {
+  try {
+    const { students, studentsByCS, studentsBySWE } = await countStudents(PATH);
+    res.write('This is the list of our students\n');
+    res.write(`Number of students: ${students.length}\n`);
+    res.write(`Number of students in CS: ${studentsByCS.length}. List: ${studentsByCS.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${studentsBySWE.length}. List: ${studentsBySWE.join(', ')}`);
 
-      res.statusCode = 200;
-      res.end();
-    }).catch((err) => {
-      res.status(500).send(err.message);
-    });
+    res.statusCode = 200;
+    res.end();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 app.listen(PORT);
