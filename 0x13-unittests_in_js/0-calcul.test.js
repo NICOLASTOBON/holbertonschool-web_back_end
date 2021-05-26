0163ce9d5e2b7bf43833c4c8123182890c378eb8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-26 15:25:31 -0500

    add files

---
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const calculateNumber = require('./0-calcul');
+const assert = require('assert');
+
+describe("calculateNumber", function () {
+  it("should round a and b and return the sum", function () {
+    assert.deepStrictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it("should round a and b and return the sum", function () {
+    assert.deepStrictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it("should round a and b and return the sum", function () {
+    assert.deepStrictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+  it("should round a and b and return the sum", function () {
+    assert.deepStrictEqual(calculateNumber(1.4, 3.4), 4);
+  } )
+})
