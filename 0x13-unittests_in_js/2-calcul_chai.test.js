30cbf99d22824a3ca3a76dea10e296b3d64c4d65
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-26 17:36:56 -0500

    add files

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const calculateNumber = require('./1-calcul');
-const { expect } = require('chai');
+const expect = require('chai').expect;
 
 describe("calculateNumber", function () {
   it("should round a and b and return by the type", function () {
