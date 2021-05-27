ea92e435bcc8d0c905bb52d32c31a2309969e363
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-27 02:57:35 -0500

    add files

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -1,15 +1,17 @@
 const { expect } = require('chai');
 const request = require('request');
 
+const URL = 'http://localhost:7865'
+
 describe('Index page', () => {
   it('Correct status code', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
+    request.get(URL, (error, response, body) => {
       expect(response.statusCode).to.equal(200);
       done()
     })
   });
   it('Correct result', (done) => {
-    request.get('http://localhost:7865', (error, response, body) => {
+    request.get(URL, (error, response, body) => {
       expect(body).to.equal('Welcome to the payment system');
       done()
     })
