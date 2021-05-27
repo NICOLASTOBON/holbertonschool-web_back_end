a9d78ede71214f291d41ab13f67e08dc46c96125
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-05-27 02:11:30 -0500

    add files

---
+++ 0x13-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,11 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('Async tests with done', (done) => {
+    getPaymentTokenFromAPI(true).then((result) => {
+      expect(result.data).to.equal('Successful response from the API');
+      done()
+    })
+  })
+})
