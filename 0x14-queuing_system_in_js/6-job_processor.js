934734e090051fab86e7cf25bee2ca5a468e5570
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2021-06-01 01:44:59 -0500

    add files

---
+++ 0x14-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,11 @@
+const kue = require('kue')
+const queue = kue.createQueue()
+
+const sendNotification = (phoneNumber, message, done) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+  done()
+}
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, done)
+})
